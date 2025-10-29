b9cce891142e3ff605b69ab018948e9001a8be43
Yulia Popova <nskgoat7@gmail.com>
2025-10-29 13:27:07 -0700

    Update test_secrets.py

--- test_secrets.py
+++ test_secrets.py
@@ -1,3 +1,6 @@
-aws_secret = "ABC123TESTSECRET"
-password = "mySecretPassword"
-token = "ghp_fake_token_123456"
+# GitHub classic PAT (40 символов: "ghp_" + 36)
+GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef1234"
+
+# AWS пары – тоже хорошо триггерят:
+AWS_ACCESS_KEY_ID = "AKIA1234567890ABCD"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
